019e9ea5c56d34084d84063a7b34732e1f878d28
Manuel <juan12292004@outlook.com>
2024-10-16 11:40:35 -0500

    feature: Auth with google: implementation domain

--- src/auth/domain/entities/user.ts
+++ src/auth/domain/entities/user.ts
@@ -1,80 +1,98 @@
 
-import {Email, Password, UserRole} from "../index";
+import { Email, Password, UserRole } from "../index";
 /**
  * Represents a user in the system.
  */
-export class User{
-    private _id?: string;
-    private _email: Email;
-    private _password: Password | null;
-    private _role: UserRole;
+export class User {
+  private _id?: string;
+  private _googleId?: string;
+  private _email: Email;
+  private _password: Password | null;
+  private _role: UserRole;
 
-    /**
-     * Creates a new User.
-     * @param {Email} email - Email of the user.
-     * @param {Password} password - Password of the user.
-     * @param {UserRole} role - Rol of the user (ej: UserRole.USER).
-     */
-    constructor(email: Email, password: Password | null, role: UserRole){
-        this._email = email;
-        this._password = password;
-        this._role = role;
+  /**
+   * Creates a new User.
+   * @param {Email} email - Email of the user.
+   * @param {Password} password - Password of the user.
+   * @param {UserRole} role - Rol of the user (ej: UserRole.USER).
+   */
+  constructor(email: Email, password: Password | null, role: UserRole, googleId?: string) {
+    this._email = email;
+    this._password = password;
+    this._role = role;
+    if (googleId) {
+      this._googleId = googleId;
     }
+  }
 
-    /**
-     * Set the id of the user.
-     * @param {string} id - Id of the user.
-     */
-    public setId(id: string): void{
-        this._id = id;
+  /**
+   * Set the id of the user.
+   * @param {string} id - Id of the user.
+   */
+  public setId(id: string): void {
+    this._id = id;
+  }
+
+  /**
+   * Get the id of the user.
+   * @returns {string} Id of the user.
+   */
+  public getId(): string | undefined {
+    return this._id;
+  }
+
+  /**
+   * Set the googleId of the user.
+   * @param {string} googleId - googleId of the user.
+   */
+  public setGoogleId(googleId: string): void {
+    this._googleId = googleId;
+  }
+
+  /**
+   * Get the googleId of the user.
+   * @returns {string} googleId of the user.
+   */
+  public getGoogleId(): string | undefined {
+    return this._googleId;
+  }
+
+
+  /**
+   * Get the email of the user.
+   * @returns {Email} Email of the user.
+   */
+  public getEmail(): Email {
+    return this._email;
+  }
+
+  /**
+   * Get the password of the user.
+   * @returns {Password} Password of the user.
+   */
+  public getPassword(): Password | null {
+    return this._password;
+  }
+
+  /**
+   * Get the role of the user.
+   * @returns {UserRole} role of the user.
+   */
+  public getRole(): UserRole {
+    return this._role;
+  }
+
+  /**
+   * Validate the password of the user.
+   * @param {string} password - Password to validate.
+   * @returns {boolean} True if the password is valid, false otherwise.
+   */
+  public validatePassword(password: string): boolean {
+    // Lógica para validar la contraseña (recordar implementar hashing)
+    if (this._password === null) {
+      return false;
     }
-    
-
-    /**
-     * Get the id of the user.
-     * @returns {string} Id of the user.
-     */
-    public getId(): string | undefined{
-        return this._id;
-    }
-
-
-
-    /**
-     * Get the email of the user.
-     * @returns {Email} Email of the user.
-     */
-    public getEmail(): Email{
-        return this._email;
-    }
-
-    /**
-     * Get the password of the user.
-     * @returns {Password} Password of the user.
-     */
-    public getPassword(): Password | null{
-        return this._password;
-    }
-
-    /**
-     * Get the role of the user.
-     * @returns {UserRole} role of the user.
-     */
-    public getRole(): UserRole{
-        return this._role;
-    }
-
-    /**
-     * Validate the password of the user.
-     * @param {string} password - Password to validate.
-     * @returns {boolean} True if the password is valid, false otherwise.
-     */
-    public validatePassword(password: string): boolean {
-        // Lógica para validar la contraseña (recordar implementar hashing)
-        if (this._password === null) {
-          return false;
-        }
-        return this._password.getValue() === password; 
-      }
+    return this._password.getValue() === password;
+  }
 }
 
--- src/auth/domain/repositories/user_repository.ts
+++ src/auth/domain/repositories/user_repository.ts
@@ -1,22 +1,28 @@
-import { User} from "..";
+import { User } from "..";
 
 /**
  * Interface representing a repository for managing user data.
  */
 export interface UserRepository {
 
-    /**
-     * Saves a regular user to the repository.
-     * @param user - The user to be saved.
-     * @returns A promise that resolves when the user is saved.
-     */
-    save(user: User): Promise<void>;
+  /**
+   * Saves a regular user to the repository.
+   * @param user - The user to be saved.
+   * @returns A promise that resolves when the user is saved.
+   */
+  save(user: User): Promise<void>;
 
-    /**
-     * Finds a regular user by their email.
-     * @param email - The email of the user to find.
-     * @returns A promise that resolves with the user if found, or null if not found.
-     */
-    findByEmail(email: string): Promise<User | null>;
-    
+  /**
+   * Finds a regular user by their email.
+   * @param email - The email of the user to find.
+   * @returns A promise that resolves with the user if found, or null if not found.
+   */
+  findByEmail(email: string): Promise<User | null>;
+
+  /**
+  * Finds a google user by their googleId.
+  * @param googleId - The googleId of the user to find.
+  * @returns A promise that resolves with the user if found, or null if not found.
+  */
+  findByGoogleId(googleId: string): Promise<User | null>;
 }
