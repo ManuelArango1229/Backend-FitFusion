baa8db38a46024d2971bfdb2a042119bd01b70d3
Manuel <juan12292004@outlook.com>
2024-12-23 11:42:38 -0500

    update

--- src/server.ts
+++ src/server.ts
@@ -25,17 +25,16 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Middleware de CORS para permitir cualquier origen y permitir el uso de credenciales
-const corsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow: boolean) => void) => {
-    // Permitir solicitudes desde cualquier origen
-    callback(null, true);
-  },
-  credentials: true,  // Permitir el uso de credenciales (cookies)
-};
 
-app.use(cors(corsOptions));
-app.options('*', cors());  
+
+
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+
+app.options('*', cors()); 
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/stats", userStatsRouter);
